9e8869d65974486f3d9d25890050b08d20ff316f
yrajm1997 <yrajm1997@gmail.com>
2023-08-04 14:19:03 +0530

    project files added

---
+++ sentiment_project/sentiment_model/processing/features.py
@@ -0,0 +1,9 @@
+from tensorflow import keras
+from keras.preprocessing.text import Tokenizer
+from sentiment_model.config.core import config
+
+# Performing the data augmentation as series of transformations
+def get_tokenizer(num_words):
+    # Tokenization
+    tokenizer = Tokenizer(num_words= num_words)    
+    return tokenizer
